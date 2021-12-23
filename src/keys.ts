8b052cda266bb702bc615f82c76ef40e3e29caf8
Erwin Flores <ramiromarca6@gmail.com>
2021-12-23 18:19:18 -0400

    add file upload download

--- src/keys.ts
+++ src/keys.ts
@@ -3,6 +3,7 @@ import {BindingKey} from '@loopback/context';
 import {User} from './models';
 import {Credentials} from './repositories';
 import {PasswordHasher} from './services';
+import {FileUploadHandler} from './types';
 
 export namespace TokenServiceConstants {
   export const TOKEN_SECRET_VALUE = 'myjwts3cr3t';
@@ -32,3 +33,15 @@ export namespace UserServiceBindings {
     'services.user.service',
   );
 }
+
+/**
+ * Binding key for the file upload service
+ */
+export const FILE_UPLOAD_SERVICE = BindingKey.create<FileUploadHandler>(
+  'services.FileUpload',
+);
+
+/**
+ * Binding key for the storage directory
+ */
+export const STORAGE_DIRECTORY = BindingKey.create<string>('storage.directory');
